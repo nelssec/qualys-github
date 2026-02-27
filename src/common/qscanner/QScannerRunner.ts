0942ac17c68090244eaef69bc8ac7c02a03df6b2
nelssec <andrew@nelssec.com>
2026-02-26 21:04:47 -0800

    Update QScanner SHA256 checksum for 4.8.0-2 GA

--- src/common/qscanner/QScannerRunner.ts
+++ src/common/qscanner/QScannerRunner.ts
@@ -18,7 +18,7 @@ import {
 
 const QSCANNER_BINARY_URL =
   'https://raw.githubusercontent.com/nelssec/qualys-lambda/main/scanner-lambda/qscanner.gz';
-const QSCANNER_SHA256 = '1a31b854154ee4594bb94e28aa86460b14a75687085d097f949e91c5fd00413d';
+const QSCANNER_SHA256 = '2d1ffb89cc97bdb19bc3f255164678319bd31a220c7028c602b7f81ba9198dba';
 
 export class QScannerRunner {
   private config: QScannerConfig;
